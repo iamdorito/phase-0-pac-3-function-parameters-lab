65d53092d7a045fe750ef08bddacb3773eb863e1
Dorothy <dorothychauu@gmail.com>
2022-11-09 21:36:13 -0500

    commit tree 53066fa859e3bd9f46f279605e8bbff81589d9ca

--- test/index-test.js
+++ test/index-test.js
@@ -1,23 +1,30 @@
 
 describe('introduction(name)', function() {
   it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
-    expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
-    expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
+    expect(introduction("Dorothy")).toEqual("Hi, my name is Dorothy.");
   })
 })
+function introduction(name) {
+  return(`Hi, my name is ${name}.`);
+}
 
 describe('introductionWithLanguage(name, language)', function() {
   it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
-    expect(introductionWithLanguage("Aki", "Ember.js")).toEqual("Hi, my name is Aki and I am learning to program in Ember.js.");
-    expect(introductionWithLanguage("Samip", "React")).toEqual("Hi, my name is Samip and I am learning to program in React.");
+    expect(introductionWithLanguage("Dorothy", "JavaScript")).toEqual("Hi, my name is Dorothy and I am learning to program in JavaScript.");
   })
 })
+function introductionWithLanguage(name, language) {
+  return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
 
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and language defaults to JavaScript', function() {
-    expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in JavaScript.");
+    expect(introductionWithLanguageOptional("Dorothy")).toEqual("Hi, my name is Dorothy and I am learning to program in JavaScript.");
   })
 })
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
 
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and the default value can be overridden with an argument', function() {
